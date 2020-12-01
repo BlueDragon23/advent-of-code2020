855e37815d36bf89080d7e92c16bef025aba9c28
Aidan Goldthorpe <agoldthorpe@atlassian.com>
2020-12-01 16:21:22 +1100

    Day 1

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -1,3 +1,18 @@
+use std::fs::File;
+use std::io::BufRead;
+use std::io::BufReader;
+
 fn main() {
-    
+    let f = File::open("input/input1_1.txt").unwrap();
+    let reader = BufReader::new(f);
+    let numbers = reader.lines().map(|s| s.unwrap().parse::<i32>().unwrap()).collect::<Vec<i32>>();
+    for a in &numbers {
+        for b in &numbers {
+            for c in &numbers {
+                if *a + *b + *c == 2020 {
+                    println!("a * b * c = {}", (*a * *b * *c));
+                }
+            }
+        } 
+    }
 }
