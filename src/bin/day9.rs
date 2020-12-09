5b8905af076487d1c3d3c4ace55bd5d2eaaade2b
Aidan Goldthorpe <agoldthorpe@atlassian.com>
2020-12-09 16:23:10 +1100

    Day 9 done. Good old fashioned adding?

---
+++ src/bin/day9.rs
@@ -0,0 +1,49 @@
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufReader;
+use std::collections::HashSet;
+use reformation::Reformation;
+
+fn main() {
+    let f = File::open("input/input9_1.txt").unwrap();
+    let reader = BufReader::new(f);
+    let numbers = reader.lines().map(|line| line.unwrap().parse::<i64>().unwrap()).collect::<Vec<i64>>();
+    let mut lower = 0;
+    let mut upper = 1;
+    let target = 90433990;
+    loop {
+        let sum: i64 = numbers.clone().into_iter().skip(lower).take(upper - lower).sum();
+        if sum == target {
+            break
+        } else if sum > target {
+            lower += 1;
+        } else {
+            upper += 1;
+        }
+    }
+    let min = numbers.clone().into_iter().skip(lower).take(upper - lower).min().unwrap();
+    let max = numbers.clone().into_iter().skip(lower).take(upper - lower).max().unwrap();
+    println!("{}, {}, {}", lower, upper, min + max);
+}
+
+fn part1(numbers: Vec<i64>) {
+    let mut index = 0;
+    let valid_numbers = numbers.clone().into_iter().skip(25).take_while(|n| {
+        // can we sum to n?
+        for a in numbers[index..(index + 25)].into_iter() {
+            for b in numbers[index..(index + 25)].into_iter() {
+                if a == b {
+                    continue;
+                }
+                if a + b == *n {
+                    index += 1;
+                    return true
+                }
+            }
+        }
+        // Update
+        false
+    }).collect::<Vec<i64>>();
+    println!("{:?}", valid_numbers);
+    println!("{}", numbers[index + 25]);
+}
